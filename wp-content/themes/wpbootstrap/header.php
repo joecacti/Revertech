6cecbc6d9def8c0dd78095e6c75a9f45986e90ec
Joey Langley <joemagnum1@hotmail.com>
2016-04-11 13:08:44 -0400

    added favicon

--- wp-content/themes/wpbootstrap/header.php
+++ wp-content/themes/wpbootstrap/header.php
@@ -17,6 +17,24 @@
     
     <!-- Link to Template CSS -->
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//favicon-16x16.png">
+<link rel="manifest" href="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="http://joeylangley.com/rts/wp-content/themes/wpbootstrap/theme-images//ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
     
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
